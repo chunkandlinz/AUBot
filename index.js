7eccaa433f691ebf0e07b280c3bab9e3c638fd78
Brian Hollis <brianhollis81@gmail.com>
2023-02-10 20:08:06 -0500

    Continuing to learn how discord bots are created.

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-const Discord = require("discord.js")
+const { token } = require("./config.json");
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
 
-require("dotenv").config()
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+client.commands = new Collection();
 
-const client = new Discord.Client({
-    intents:
-    [
-        "Guilds",
-        "GuildMessages"
-    ]
-})
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`)
-})
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
 
-client.login(process.env.TOKEN)
+client.login(token);
